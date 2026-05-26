66f4282258b053e7ca97212d37e8c7065bc00c4e
sara-jpallander <sara.j.pallander@gmail.com>
2026-05-26 15:41:12 +0200

    front page content and styling

--- palatable-frontend/src/App.tsx
+++ palatable-frontend/src/App.tsx
@@ -8,8 +8,21 @@ function App() {
     <>
     <HeaderSection />
     <main>
-      <div className="SiteContainer">
-          <h1>Welcome</h1>
+      <div className="SiteContainer siteGrid">
+          <h1 className='sitegrid-halfWidth--left'>Welcome</h1>
+          <p className="FrontPage-slogan sitegrid-halfWidth--left">Let us set the tone.</p>
+
+          <section className='FrontPage-intro sitegrid-halfWidth--left'>
+            <h2>pal · at · a · ble</h2>
+            <p>
+              Meaning acceptable and pleasing to the taste and mind. <br/>  
+              Here at Palatable we strive to set the palette for your projects with good aesthetics in mind, 
+              without compromising on accessibility.
+            </p>
+            <button>
+              How it works <span>🡢</span>
+            </button>
+          </section>
       </div>
     </main>
     </>
