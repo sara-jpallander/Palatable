d2f47bcff8ac3e6ef28370c8e91e9e98ed4680f3
FelixOS <puffis.stenersjo@gmail.com>
2026-05-27 14:17:58 +0200

    added react router and moved mainPage code to mainPage

---
+++ palatable-frontend/src/pages/mainPage.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const mainPage = () => {
+  return (
+    <>
+    <main>
+    <div className="SiteContainer siteGrid">
+        <h1 className='sitegrid-halfWidth--left'>Welcome</h1>
+        <p className="FrontPage-slogan sitegrid-halfWidth--left">Let us set the tone.</p>
+
+        <section className='FrontPage-intro sitegrid-halfWidth--left'>
+          <h2>pal · at · a · ble</h2>
+          <p>
+            Meaning acceptable and pleasing to the taste and mind. <br/>  
+            Here at Palatable we strive to set the palette for your projects with good aesthetics in mind, 
+            without compromising on accessibility.
+          </p>
+          <button>
+            How it works <span>🡢</span>
+          </button>
+        </section>
+    </div>
+  </main>
+  </>
+  )
+}
+
+export default mainPage
